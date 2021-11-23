2e35143af265dc63f0cfaa17f2ed7b69ac5bb969
Mihir5897 <mbhawsar@isystango.com>
2021-11-23 14:14:08 +0530

    add new update file in mihir Branch

---
+++ app/src/main/java/com/me/systangoandorid/update.java
@@ -0,0 +1,15 @@
+package com.me.systangoandorid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class update extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update);
+        //add new file in MIHIR Branch
+    }
+}

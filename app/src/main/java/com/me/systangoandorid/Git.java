b6af56d55edc7a7fe0b80a241f0c36131ceadb2a
Mihir5897 <mbhawsar@isystango.com>
2021-11-23 13:01:58 +0530

    Checking file to git operation

---
+++ app/src/main/java/com/me/systangoandorid/Git.java
@@ -0,0 +1,15 @@
+package com.me.systangoandorid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Git extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git);
+    }
+    //this file create for git operation
+}
